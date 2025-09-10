f2d7bf3d971cb8f18b477d44e1a556d0d7e5766f
ynotopec <49277296+ynotopec@users.noreply.github.com>
2025-09-10 11:09:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 serverAddress=$1
 portNumber=$2
 
-pythonVersion=python${PYTHON_VERSION:-3}
+pythonVersion=python3
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pythonDir=~/"venv/$(basename "${DIR}")"
@@ -16,19 +16,35 @@ source "${pythonDir}"/bin/activate
 
 #intall
 ${pythonVersion} -m pip cache purge
-${pythonVersion} -m pip install -U pip
+${pythonVersion} -m pip install -U pip setuptools wheel
 ${pythonVersion} -m pip install -U -r requirements.txt
 #optimize space
-(jdupes -X size+:99M -r -L ~ >/dev/null 2>&1 )&
+#(jdupes -X size+:99M -r -L ~ >/dev/null 2>&1 )&
 
-export OPENAI_API_MODEL="vicuna"
-export OPENAI_API_BASE="https://api-ai.numerique-interieur.com/v1"
-export OPENAI_API_KEY="sk-1234"
-
-[ ! -z "${serverAddress}" ] && export GRADIO_SERVER_NAME="${serverAddress}"
-[ ! -z "${portNumber}" ] && export GRADIO_SERVER_PORT="${portNumber}"
+export HF_HUB_DISABLE_TELEMETRY=1
+if [ ! -z "${serverAddress}" ] ;then
+  export GRADIO_SERVER_NAME="${serverAddress}"
+  export SERVER_NAME="${serverAddress}"
+fi
+if [ ! -z "${portNumber}" ] ;then
+  export GRADIO_SERVER_PORT="${portNumber}"
+  export SERVER_PORT="${portNumber}"
+  export BACK_PORT=$((SERVER_PORT + 1))
+fi
 export CUDA_LAUNCH_BLOCKING=1
 
-${pythonVersion} app.py
+# Charger les variables d'environnement depuis .env
+if [ -f ".env" ]; then
+#  export $(grep -v '^#' .env | xargs)
+  set -a
+  source .env
+  set +a
+else
+  echo ".env file not found!"
+  exit 1
+fi
+
+${pythonVersion} app.py $([ ! -z "${serverAddress}" ] && echo --host ${serverAddress}) $([ ! -z "${portNumber}" ] && echo --port ${portNumber})
 #${pythonVersion} -m streamlit run app.py --browser.gatherUsageStats false $([ ! -z "${serverAddress}" ] && echo --server.address ${serverAddress}) $([ ! -z "${portNumber}" ] && echo --server.port ${portNumber})
 #${pythonVersion} -m uvicorn app:app --reload $([ ! -z "${serverAddress}" ] && echo --host ${serverAddress}) $([ ! -z "${portNumber}" ] && echo --port ${portNumber})
+#${pythonVersion} back.py
