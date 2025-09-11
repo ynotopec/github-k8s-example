412974b0d8672aa650e31782a3a89eed4e531b28
ynotopec <49277296+ynotopec@users.noreply.github.com>
2025-09-11 14:43:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def hello_world():
 
 if __name__ == "__main__":
     host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", "5000"))
+    port = int(os.getenv("PORT", "8080"))
     app.run(host=host, port=port)
